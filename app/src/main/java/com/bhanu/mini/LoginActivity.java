12b830f258c7322bdbef6919f3ecde24670aefd9
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-09-24 00:12:31 +0100

    wip

--- app/src/main/java/com/bhanu/mini/LoginActivity.java
+++ app/src/main/java/com/bhanu/mini/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.bhanu.mini;
 
 import android.content.ActivityNotFoundException;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.StrictMode;
 import android.speech.RecognizerIntent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,12 +13,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -29,8 +33,9 @@ public class LoginActivity extends AppCompatActivity {
     final int PASSWORD_VOICE_CODE = 200;
     private Button btnSubmit;
     public EditText editTxtFrom, editTxtPwd;
-    String From = null, Pwd = null;
+    String from = null, Pwd = null;
 
+    SendMailSSL sendMailSSL;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(com.bhanu.mini.R.layout.activity_login);
+        sendMailSSL = new SendMailSSL();
 
         if (Build.VERSION.SDK_INT > 9) {
 
@@ -48,13 +54,6 @@ public class LoginActivity extends AppCompatActivity {
         editTxtFrom = (EditText) findViewById(com.bhanu.mini.R.id.txt_usr);
         editTxtPwd = (EditText) findViewById(com.bhanu.mini.R.id.txt_pwd);
         btnSubmit = (Button) findViewById(com.bhanu.mini.R.id.btn_sbt);
-
-        Toolbar t = (Toolbar) findViewById(com.bhanu.mini.R.id.tob);
-        t.setNavigationIcon(com.bhanu.mini.R.drawable.ic_action_name4);
-        t.setTitle("Login");
-        getSupportActionBar().setIcon(com.bhanu.mini.R.drawable.ic_action_name2);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        // getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         inputOnClickListener(editTxtFrom, EMAIL_VOICE_CODE);
         inputOnClickListener(editTxtPwd, PASSWORD_VOICE_CODE);
@@ -69,62 +68,47 @@ public class LoginActivity extends AppCompatActivity {
                 if (editTxtFrom.getText().toString().contains("@") && (editTxtFrom.getText().toString().toLowerCase().contains("gmail.com") || editTxtFrom.getText().toString().toLowerCase().contains("yahoo.co"))) {
                     if (!editTxtPwd.getText().toString().equalsIgnoreCase("")) {
 
-                        From = editTxtFrom.getText().toString();
+                        from = editTxtFrom.getText().toString();
                         Pwd = editTxtPwd.getText().toString();
+                        sendMailSSL.sendMail(from,Pwd);
 
 
 
-                            Properties props = new Properties();
-                            //Toast.makeText(getApplicationContext(),From+" "+Pwd+" Entered Port Authentication",Toast.LENGTH_LONG).show();
+                        Properties props = new Properties();
+                        //Toast.makeText(getApplicationContext(),from+" "+Pwd+" Entered Port Authentication",Toast.LENGTH_LONG).show();
 
-                            if (From.toLowerCase().contains("gmail")) {
+                        if (from.toLowerCase().contains("gmail")) {
 
-                                //Get the session object
-                                //  Toast.makeText(getApplicationContext(),"Entered Port Authentication",Toast.LENGTH_LONG).show();
-                                props.put("mail.smtp.auth", "true");
-                                props.put("mail.smtp.starttls.enable", "true");
-                                props.put("mail.smtp.host", "smtp.gmail.com");
-                                props.put("mail.smtp.port", "587");
+                            //Get the session object
+                            //  Toast.makeText(getApplicationContext(),"Entered Port Authentication",Toast.LENGTH_LONG).show();
+                            props.put("mail.smtp.auth", "true");
+                            props.put("mail.smtp.starttls.enable", "true");
+                            props.put("mail.smtp.host", "smtp.gmail.com");
+                            props.put("mail.smtp.port", "587");
 
 
-                            } else if (From.toLowerCase().contains("yahoo")) {
-                                props.put("mail.smtp.host", "smtp.mail.yahoo.com");
-                                props.put("mail.smtp.socketFactory.port", "587");
-                                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-                                props.put("mail.smtp.auth", "true");
-                                props.put("mail.smtp.port", "587");
+                        } else if (from.toLowerCase().contains("yahoo")) {
+                            props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+                            props.put("mail.smtp.socketFactory.port", "587");
+                            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                            props.put("mail.smtp.auth", "true");
+                            props.put("mail.smtp.port", "587");
+                        }
+
+                        Session session = Session.getInstance(props, new Authenticator() {
+                            protected PasswordAuthentication getPasswordAuthentication() {
+
+                                return new PasswordAuthentication(from, Pwd);//change accordingly
                             }
-
-
-                            //   Properties props = new Properties();
-
-
-
-
-                            Session session = Session.getInstance(props, new Authenticator() {
-                                protected PasswordAuthentication getPasswordAuthentication() {
-
-                                    return new PasswordAuthentication(From, Pwd);//change accordingly
-                                }
-                            });
+                        });
                         try {
-
-                            //compose message
-                            MimeMessage message = new MimeMessage(session);
-                            message.setFrom(new InternetAddress(From));//change accordingly
-                            message.addRecipient(Message.RecipientType.TO, new InternetAddress(From));
-                            message.addRecipient(Message.RecipientType.CC, new InternetAddress(From));
-                            message.setSubject("Voice Based Mail Login.");
-                            message.setText("We respect Your Privacy.");
-                            //
-                            //send message
-                            Transport.send(message);
-                            //        Toast.makeText(getApplicationContext(),"Authenticating Your Login!!!",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Authenticating, We respect your privacy", Toast.LENGTH_LONG).show();
 
                             Intent i = new Intent(LoginActivity.this, VoiceEmailActivity.class);
-                            i.putExtra("FROM", From);
+                            i.putExtra("FROM", from);
                             i.putExtra("PWD", Pwd);
                             startActivity(i);
+
                         } catch (Exception e) {
 
 
@@ -144,30 +128,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-
-    private void inputOnClickListener(EditText eText, final int code) {
-
-        eText.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-
-                try {
-                    startActivityForResult(intent, code);
-                } catch (ActivityNotFoundException a) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Ops! Your device doesn't support Speech to Text",
-                            Toast.LENGTH_SHORT);
-                    t.show();
-                }
-            }
-        });
-
     }
 
     private void textChange(final EditText eText) {
@@ -202,8 +162,33 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void inputOnClickListener(EditText editText, final int code) {
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try {
+                    startActivityForResult(intent, code);
+                } catch (ActivityNotFoundException a) {
+                    Snackbar.make(view, "Oops! Your device doesn't support Speech to Text", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        System.out.print(requestCode);
+        System.out.print(resultCode);
+
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case EMAIL_VOICE_CODE: {
@@ -212,12 +197,12 @@ public class LoginActivity extends AppCompatActivity {
                     ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     for (String result : results) {
-                        if (!result.equalsIgnoreCase("clear")) {
+                        if (!result.equalsIgnoreCase("clean")) {
                             try {
 
                                 editTxtFrom.setText(result);
 
-                                From = editTxtFrom.getText().toString();
+                                from = editTxtFrom.getText().toString();
 
                             } catch (Exception e) {
 
--- app/src/main/java/com/bhanu/mini/VoiceEmailActivity.java
+++ app/src/main/java/com/bhanu/mini/VoiceEmailActivity.java
@@ -2,8 +2,11 @@ package com.bhanu.mini;
 
 import java.util.ArrayList;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
@@ -16,38 +19,46 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.Properties;
+
 import javax.mail.*;
 import javax.mail.internet.*;
 
 public class VoiceEmailActivity extends AppCompatActivity {
 
-    protected static final  int RESULT_SPEECH = 1;
+    protected static final int RESULT_SPEECH = 1;
+    protected static final int SEND_CODE = 200;
 
-    private ImageButton btnSpeakTO,btnSpeakCC,btnSpeakSub,btnSpeakEB;
+    private ImageButton btnSpeakCC, btnSpeakSub, btnSpeakEB;
+    private FloatingActionButton btnSpeakTO;
     private TextView txtText;
-    public EditText editTxtTO,editTxtCC,editTxtSub,editTxtEB;
+    public EditText editTxtTO, editTxtCC, editTxtBCC, editTxtSub, editTxtEB;
     // private Button btnSendE
     //SendMailSSL smssl;
-    String fr=null,toadd=null,txt_cc=null,txt_sub=null,txt_eb=null,txt_to=null,dd=null,ddd=null;
+    String fr = null, password = null, txt_cc = null, txt_sub = null, txt_eb = null, txt_to = null, dd = null, ddd = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.bhanu.mini.R.layout.activity_email);
         getSupportActionBar().setIcon(com.bhanu.mini.R.drawable.ic_action_name2);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        if(Build.VERSION.SDK_INT>9) {
+        if (Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
         //txtText = (TextView) findViewById(R.id.txtText);
-        editTxtTO = (EditText) findViewById(com.bhanu.mini.R.id.txt_to);
-        editTxtCC = (EditText) findViewById(com.bhanu.mini.R.id.txt_cc);
-        editTxtSub = (EditText) findViewById(com.bhanu.mini.R.id.txt_sub);
-        editTxtEB = (EditText) findViewById(com.bhanu.mini.R.id.txt_bdy);
-        btnSpeakTO = (ImageButton) findViewById(com.bhanu.mini.R.id.voice_btn);
+        editTxtTO = (EditText) findViewById(R.id.to_email);
+        editTxtCC = (EditText) findViewById(R.id.cc_email);
+        editTxtBCC = (EditText) findViewById(R.id.bcc_email);
+        editTxtSub = (EditText) findViewById(R.id.subject_email);
+        editTxtEB = (EditText) findViewById(R.id.body_email);
+
+        btnSpeakTO = (FloatingActionButton) findViewById(com.bhanu.mini.R.id.send_button);
 
         //btnSendEmail = (Button)findViewById(R.id.btnEmail
+        ClickListener.inputOnClickListener(editTxtTO, RESULT_SPEECH);
 
 
         editTxtTO.setOnClickListener(new OnClickListener() {
@@ -136,205 +147,48 @@ public class VoiceEmailActivity extends AppCompatActivity {
         });
 
 
-
         btnSpeakTO.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(
-                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-
-                try {
-                    startActivityForResult(intent, RESULT_SPEECH);
-
-
-                    //txtText.setText("");
-                } catch (ActivityNotFoundException a) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Ops! Your device doesn't support Speech to Text",
-                            Toast.LENGTH_SHORT);
-                    t.show();
-                }
+                sendEmail();
             }
         });
-
-
-
-
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-
-        switch (requestCode) {
-            case RESULT_SPEECH: {
-
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> text = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    if(text.get(0).equalsIgnoreCase("send email")){
-                        try {
-                            Toast.makeText(getApplicationContext(),"Page Loading!!!" ,Toast.LENGTH_LONG).show();;
-
-                            fr = this.getIntent().getStringExtra("FROM");
-                            toadd = this.getIntent().getStringExtra("PWD");
-                            txt_to = editTxtTO.getText().toString();
-                            txt_cc = editTxtCC.getText().toString();
-                            txt_sub = editTxtSub.getText().toString();
-                            txt_eb = editTxtEB.getText().toString();
-                            // Toast.makeText(getApplicationContext(),fr+", "+toadd+", "+txt_to+", "+txt_cc+", "+
-                            //       txt_sub+", "+txt_eb,Toast.LENGTH_LONG).show();
-                            //smssl = new SendMailSSL(fr,toadd, txt_to, txt_cc, txt_sub,txt_eb);
-                            //sendEmail();
-
-
-                                Properties props = new Properties();
-
-                                if (fr.contains("gmail")) {
-                                    //Get the session object
-                                    Toast.makeText(getApplicationContext(),"Entered Port Authentication",Toast.LENGTH_LONG).show();
-                                    props.put("mail.smtp.auth", "true");
-                                    props.put("mail.smtp.starttls.enable", "true");
-                                    props.put("mail.smtp.host", "smtp.gmail.com");
-                                    props.put("mail.smtp.port", "587");
-                                } else if (fr.contains("yahoo")) {
-                                    Toast.makeText(getApplicationContext(),"Entering Port Authentication",Toast.LENGTH_LONG).show();
-
-                                    props.put("mail.smtp.host", "smtp.mail.yahoo.com");
-                                    props.put("mail.smtp.socketFactory.port", "587");
-                                    props.put("mail.smtp.socketFactory.class",
-                                            "javax.net.ssl.SSLSocketFactory");
-                                    props.put("mail.smtp.auth", "true");
-                                    props.put("mail.smtp.port", "587");
-                                }
-
-
-                                Session session = Session.getDefaultInstance(props,
-                                        new Authenticator() {
-                                            protected PasswordAuthentication getPasswordAuthentication() {
-                                                return new PasswordAuthentication(fr, toadd);//change accordingly
-                                            }
-                                        });
-
-                                //compose message
-                                txt_to = txt_to.replaceAll(" ","");
-                                txt_cc = txt_cc.replaceAll(" ","");
-                                txt_eb=txt_eb.replaceAll("full stop",".");
-                                txt_eb=txt_eb.replaceAll("dot",".");
-                                txt_eb=txt_eb.replaceAll("Dot",".");
-                                txt_eb=txt_eb.replaceAll("question mark","?");
-                                txt_eb=txt_eb.replaceAll("questionmark","?");
-                                txt_eb=txt_eb.replaceAll("semicolon",";");
-                                txt_eb=txt_eb.replaceAll("semi colon",";");
-                                txt_eb=txt_eb.replaceAll("exclamation","!");
-                                txt_eb=txt_eb.replaceAll("colon",":");
-                                txt_eb=txt_eb.replaceAll("hyphen","-");
-                                txt_eb=txt_eb.replaceAll("dash","-");
-                                txt_eb=txt_eb.replaceAll("Dash","-");
-                                txt_eb=txt_eb.replaceAll("underscore","_");
-                                txt_eb=txt_eb.replaceAll("next line","\n");
-                                txt_eb=txt_eb.replaceAll("comma",",");
-                                if(txt_cc==null){
-                                    txt_cc = " ";
-                                }
-
-                                Toast.makeText(getApplicationContext(),fr + " From Address " + txt_to + " To Address",Toast.LENGTH_LONG).show();
-
-                                MimeMessage message = new MimeMessage(session);
-                                message.setFrom(new InternetAddress(fr));//change accordingly
-                                message.addRecipient(Message.RecipientType.TO, new InternetAddress(txt_to));
-                            if(txt_cc.trim().equals("")) {
-
-                            } else{
-
-                                message.addRecipient(Message.RecipientType.CC, new InternetAddress(txt_cc));
-                            }
-
-            message.setSubject(txt_sub);
-            message.setText(txt_eb);
-
-            //send message to
-            Transport.send(message);
-
-
-                            Toast.makeText(getApplicationContext(),"Email Sent Successfully" ,Toast.LENGTH_LONG).show();
-                            
-                            Intent gmailIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
-
-                            if(gmailIntent != null)
-                                startActivity(gmailIntent);
-                        }catch(Exception e){
-                            //e.printStackTrace();
-                            Toast.makeText(getApplicationContext(),"Email Could not be Sent" + e.getMessage() ,Toast.LENGTH_LONG).show();;
-                        }
-                    }
-
-                    else if(editTxtTO.findFocus()==editTxtTO){
-                        editTxtTO.append(text.get(0));
-                     break;
-
-                    }
-                    else if(editTxtCC.findFocus() ==editTxtCC){
-                        editTxtCC.append(text.get(0));
-                     break;
-                    }
-                    else if(editTxtSub.findFocus()==editTxtSub){
-                        editTxtSub.append(text.get(0));
-                     break;
-                    }
-                    else if(editTxtEB.findFocus()==editTxtEB){
-                        editTxtEB.append(text.get(0));
-                        break;
-                    }
-
-                    //editTxt.append(text.get(0));
-
-                }
-                break;
-            }
-
-        }
-    }
     protected void sendEmail() {
-        /*Log.i("Send email", "");
-        String[] TO = {editTxtTO.getText().toString()};
-        String[] CC = {editTxtCC.getText().toString()};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
+
+        String packageName = "com.google.android.gm";
+
+        String toEmail = editTxtTO.getText().toString();
+        String ccEmail = editTxtCC.getText().toString();
+        String bccEmail = editTxtBCC.getText().toString();
+
+        String[] TO = toEmail.split("\\,");
+        String[] CC = ccEmail.split("\\,");
+        String[] BCC = bccEmail.split("\\,");
+
+        txt_sub = editTxtSub.getText().toString();
+        txt_eb = editTxtEB.getText().toString();
 
 
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, editTxtSub.getText().toString());
-        emailIntent.putExtra(Intent.EXTRA_TEXT, editTxtEB.getText().toString());
+        fr = this.getIntent().getStringExtra("FROM");
+        password = this.getIntent().getStringExtra("PWD");
 
         try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            finish();
-            Log.i("Finished sending email...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(VoiceEmailActivity.this,
-                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
-        }*/
-        try {
-            Properties props = new Properties();
+            Properties props = System.getProperties();
 
             if (fr.contains("gmail")) {
-//Get the session object
-                Toast.makeText(getApplicationContext(),"Entered Port Authentication",Toast.LENGTH_LONG).show();
+                //Get the session object
                 props.put("mail.smtp.auth", "true");
                 props.put("mail.smtp.starttls.enable", "true");
                 props.put("mail.smtp.host", "smtp.gmail.com");
                 props.put("mail.smtp.port", "587");
             } else if (fr.contains("yahoo")) {
-                Toast.makeText(getApplicationContext(),"Entering Port Authentication",Toast.LENGTH_LONG).show();
+
+                packageName ="com.yahoo.mobile.client.android.mail";
 
                 props.put("mail.smtp.host", "smtp.mail.yahoo.com");
                 props.put("mail.smtp.socketFactory.port", "587");
@@ -344,37 +198,45 @@ public class VoiceEmailActivity extends AppCompatActivity {
                 props.put("mail.smtp.port", "587");
             }
 
-
-            Session session = Session.getDefaultInstance(props,
-                    new Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(fr, toadd);//change accordingly
-                        }
-                    });
+            Session session = Session.getDefaultInstance(props, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fr, password);//change accordingly
+                }
+            });
 
             //compose message
-            txt_to = txt_to.replaceAll(" ","");
-            txt_cc = txt_cc.replaceAll(" ","");
-            Toast.makeText(getApplicationContext(),fr + " From Address " + txt_to + " To Address",Toast.LENGTH_LONG).show();
-            /*MimeMessage message = new MimeMessage(session);
+            MimeMessage message = new MimeMessage(session);
+
             message.setFrom(new InternetAddress(fr));//change accordingly
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(txt_to));
-            message.addRecipient(Message.RecipientType.CC, new InternetAddress(txt_cc));
+
+            for (String email : TO) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            }
+
+            for (String email : CC) {
+                message.addRecipient(Message.RecipientType.CC, new InternetAddress(email));
+            }
+            for (String email : BCC) {
+                message.addRecipient(Message.RecipientType.BCC, new InternetAddress(email));
+            }
+
             message.setSubject(txt_sub);
             message.setText(txt_eb);
 
             //send messageti
-            Transport.send(message);*/
+            Transport.send(message);
+            Toast.makeText(getApplicationContext(), "Email Sent Successfully", Toast.LENGTH_LONG).show();
 
-            Intent iemail = new Intent(Intent.ACTION_SEND);
-            iemail.putExtra(Intent.EXTRA_EMAIL,txt_to);
-            iemail.putExtra(Intent.EXTRA_CC,txt_cc);
-            iemail.putExtra(Intent.EXTRA_SUBJECT,txt_sub);
-            iemail.putExtra(Intent.EXTRA_TEXT,txt_eb);
-            startActivity(Intent.createChooser(iemail,""));
+            if (fr.contains("gmail")) {
+                Intent mailIntent = getPackageManager().getLaunchIntentForPackage(packageName);
 
-        }catch(Exception e){
-            e.printStackTrace();
+                if (mailIntent != null)
+                    startActivity(mailIntent);
+            }
+
+        } catch (Exception e) {
+            //e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Email Could not be Sent" + e.getMessage() ,Toast.LENGTH_LONG).show();;
         }
     }
 }
